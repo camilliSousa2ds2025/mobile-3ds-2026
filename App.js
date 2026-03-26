a592d9ba38feeed6926c1646951420534b519a86
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2026-03-26 09:19:38 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
-//import Aula01 from "./src/aula01";
-//import Aula02 from "./src/aula02";
-
-import  Aula03 from ".src/aula03";
+import Aula01 from "./src/aula01";
+import Aula02 from "./src/aula02";
 
 export default function App() {
   return (
     //<Aula01 />
-    //<Aula02 />
-    <Aula03 />
+    <Aula02 />
   );
 }
 
