cc166e7a1cbb025859fd89c3d76e6a962a475482
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2026-05-26 10:33:25 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI  || 'mongodb://localhost:27016/react-native';
+const uri = 'mongodb://127.0.0.1:27016';
 
-const client = new MongoClient(uri);
+const dbName = 'banco1';
+
+async function conectar() {
+    try {
+        await client.connect();
+        console.log('Conexão com o MongoDB estabelecida!');
+
+        const db = client.db();
+        const colecoes = await db.listCollections().toArray();
+        console.log('Coleções encontradas: ', colecoes.length);
+    }catch (err){
+        console.log('Falha ao conectar: ', err.message)
+    }
+}
+
+conectar();
